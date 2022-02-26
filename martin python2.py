650d039f2f1c48d086218561f0b2247b5064a1c7
MartinAj23 <99495880+MartinAj23@users.noreply.github.com>
2022-02-26 11:45:10 +0100

    Add files via upload

---
+++ martin python2.py
@@ -0,0 +1,33 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (value)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print (value)
+NameError: name 'value' is not defined
+>>> "<pyshell#1>"
+'<pyshell#1>'
+>>> print"BLESSING"
+SyntaxError: invalid syntax
+>>> print "CATHERINE"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("CATHERINE")?
+>>> 'print' ("CATHERINE")
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    'print' ("CATHERINE")
+TypeError: 'str' object is not callable
+>>> print
+<built-in function print>
+>>> <built-in function print>
+SyntaxError: invalid syntax
+>>> print ("hello world")
+hello world
+>>> "copyright"
+'copyright'
+>>> print "Print"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Print")?
+>>> print ("Hello world trent")
+Hello world trent
+>>> print ("MNM")
+MNM
+>>> 
